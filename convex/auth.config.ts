b43dc3b03d4ed79b106b8de67669ba09b5056333
Christopher-CBS <christopherbadibanga2014>
2024-08-09 00:09:07 +0200

    first commit

---
+++ convex/auth.config.ts
@@ -0,0 +1,12 @@
+
+const authConfig =  {
+    providers: [
+      {
+        domain: "https://trusting-wallaby-57.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
+
+
+export default authConfig;
